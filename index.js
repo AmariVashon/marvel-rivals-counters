22ad172a44a50dabc69b152a36210f8d5f27e808
amarivashon <amarivashon@gmail.com>
2026-05-26 19:34:08 -0400

    Basic File Structure and Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const supabaseClient = require('@supabase/supabase-js');
+const dotenv = require('dotenv');
+
+const app = express();
+const port = 3000;
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+
+const API_KEY = process.env.API_KEY;
+
+
+app.get('/', (req, res) => {
+  res.sendFile('public/main.html', { root: __dirname });
+});
+
+app.listen(port, () => {
+  console.log(`App is available on port: ${port}`);
+});
